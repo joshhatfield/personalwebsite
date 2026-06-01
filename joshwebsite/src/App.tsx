b81dce0bfe1cd566321b374e0b11edc7f2b94868
joshhatfield <josh.hatfeiled@gmail.com>
2026-06-01 17:26:57 +1000

    blog fixes

--- joshwebsite/src/App.tsx
+++ joshwebsite/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import BlogListPage from "./pages/blog/BlogList.tsx";
 import BlogArticlePage from "./pages/blog/BlogArticle.tsx";
@@ -8,13 +8,13 @@ const articles = [BudgetAiArticle];
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<BlogListPage articles={articles} />} />
         <Route path="/blog/smart-ai-on-a-budget" element={<BlogArticlePage article={BudgetAiArticle} />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
